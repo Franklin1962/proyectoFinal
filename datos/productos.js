b456c6d9222ed8f496e04925430cf1223e94cc3f
Franklin Pereira <pereirafranklin25@gmail.com>
2024-04-29 22:27:46 -0300

    segundo comit

---
+++ datos/productos.js
@@ -0,0 +1,63 @@
+let productos = [
+  {id: 1, nombre: "harina PAN",
+      descripcion: "Harina de Maiz Blanco precocida",
+      precio: 9.99,
+      imagen:"../public/imagenes/img01.jpg"
+      },
+
+  {id: 2,
+    nombre: "harina PAN",
+    descripcion: "Mescla para Cachapas",
+    precio: 9.99,
+    imagen:"../public/imagenes/img02.jpg"
+  },
+
+  {id : 3,
+    nombre: "Diablitos Underwood",
+    descripcion: "Jamon Endiablado",
+    precio: 9.99,
+    imagen:"../public/imagenes/img03.jpg"
+  },
+
+  {id: 4,
+    nombre: "Rikesa",
+    descripcion: "Queso Cheddar fundido",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img04.jpg"
+  },
+
+  {id: 5,
+    nombre: "Cheez Whiz",
+    descripcion: "Queso Cheddar fundido",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img05.jpg"
+  },
+
+  {id: 6,
+    nombre: "Margarina Mavesa",
+    descripcion: "Margarina para untar",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img06.jpg"
+  },
+
+  {id: 7,
+    nombre: "Margarina Nelly",
+    descripcion: "Margarina para untar",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img07.jpg" 
+  },
+  {id: 8,
+    nombre: "Mayonesa Kraff",
+    descripcion: "Mayonesa",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img08.jpg"
+  },
+
+  {id: 9,
+    nombre: "Mayonesa Mavesa",
+    descripcion: "Mayonesa",  
+    precio: 9.99,
+    imagen:"../public/imagenes/img09.jpg"
+  }
+
+]
